e509b7f208ad3f4a7fd1a1c760140a80f4633fe6
Julio Torres <jtorres990@gmail.com>
2024-11-07 14:59:44 -0500

    Refactor root component: remove unused font links and optimize font loading

--- app/root.tsx
+++ app/root.tsx
@@ -11,18 +11,7 @@ import type { LinksFunction, LoaderFunction } from "@remix-run/node";
 import "@/tailwind.css";
 import LandingContext from "./contexts/landing";
 
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+export const links: LinksFunction = () => [];
 
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
@@ -31,12 +20,15 @@ export const loader: LoaderFunction = async ({ request }) => {
     blogUrl: process.env.BLOG_URL,
     enableChat: process.env.ENABLE_CHAT === "true",
     googleAnalyticsId: process.env.GOOGLE_ANALYTICS_ID,
-    canonical: `${protocol ? protocol + ':' : url.protocol}//${url.host}${url.pathname}`,
+    canonical: `${protocol ? protocol + ":" : url.protocol}//${url.host}${
+      url.pathname
+    }`,
   });
 };
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  const { canonical, googleAnalyticsId, blogUrl, enableChat } = useLoaderData<typeof loader>();
+  const { canonical, googleAnalyticsId, blogUrl, enableChat } =
+    useLoaderData<typeof loader>();
 
   return (
     <html lang="en">
@@ -44,11 +36,32 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link href={canonical} rel="canonical" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          as="style"
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          onLoad={(evt) => {
+            evt.currentTarget.rel = "stylesheet";
+          }}
+        />
+        <noscript>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          />
+        </noscript>
         <Meta />
         <Links />
 
         <script
           async
+          defer
           src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
         ></script>
         <script
