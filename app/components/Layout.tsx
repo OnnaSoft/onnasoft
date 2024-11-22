c8c985ca3cf779099d03cb2a22a2cb8b377044c0
Julio Torres <jtorres990@gmail.com>
2024-11-21 22:37:43 -0500

    Refactor Layout component to improve type safety by defining LayoutProps type for props

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import Footer from "~/components/Footer";
 import Navigation from "./Navigation";
 
-export default function Layout({ children }: React.PropsWithChildren) {
+type LayoutProps = Readonly<React.PropsWithChildren>;
+
+export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900">
       <Navigation />
