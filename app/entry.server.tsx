c2a13967d922d00f78ecb3d069ec94e6ba6c5081
Julio Torres <jtorres990@gmail.com>
2024-11-21 21:28:20 -0500

    Add security headers to enhance response security and improve content policies

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -7,6 +7,25 @@ import { renderToPipeableStream } from "react-dom/server";
 
 const ABORT_DELAY = 5_000;
 
+const securityHeaders = {
+  "Content-Type": "text/html",
+  "Content-Security-Policy":
+    "default-src 'self'; img-src 'self' https:; script-src 'self' 'unsafe-inline';",
+  "X-Content-Type-Options": "nosniff",
+  "X-Frame-Options": "DENY",
+  "Strict-Transport-Security": "max-age=31536000; includeSubDomains; preload",
+  "Referrer-Policy": "strict-origin-when-cross-origin",
+  "Permissions-Policy": "geolocation=(), microphone=(), camera=()",
+  "X-Permitted-Cross-Domain-Policies": "none",
+  "Cross-Origin-Resource-Policy": "same-origin",
+  "Cross-Origin-Embedder-Policy": "require-corp",
+  "Cross-Origin-Opener-Policy": "same-origin",
+  "Cache-Control": "public, max-age=2592000, s-maxage=2592000",
+  "Set-Cookie": "sessionId=abc123; HttpOnly; Secure; SameSite=Strict",
+  Server: "",
+  "Expect-CT": "max-age=86400, enforce",
+};
+
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
@@ -28,11 +47,10 @@ export default function handleRequest(
           const body = new PassThrough();
           const stream = createReadableStreamFromReadable(body);
 
-          responseHeaders.set("Content-Type", "text/html");
-          responseHeaders.set(
-            "Cache-Control",
-            "public, max-age=2592000, s-maxage=2592000"
-          );
+          // Establece todas las cabeceras en un solo paso
+          Object.entries(securityHeaders).forEach(([key, value]) => {
+            responseHeaders.set(key, value);
+          });
 
           resolve(
             new Response(stream, {
